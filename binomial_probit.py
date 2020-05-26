e1fdce34fab34199a14bbdf95d5ee4bcc9070429
Martin Ingram <ingramm@student.unimelb.edu.au>
2020-05-26 17:17:24 +1000

    Added tennis example with illustration

--- binomial_probit.py
+++ binomial_probit.py
@@ -1,11 +1,11 @@
-import jax.numpy as jnp
-from jax.scipy.stats import norm
+import numpy as np
+from scipy.stats import norm
 from utils import pdf_cdf_ratio_grad, pdf_cdf_ratio
 
 
 def likelihood(x, n, p):
 
-    return jnp.sum(p * norm.logcdf(x) + (n - p) * norm.logcdf(-x))
+    return np.sum(p * norm.logcdf(x) + (n - p) * norm.logcdf(-x))
 
 
 def likelihood_grad(x, n, p):
